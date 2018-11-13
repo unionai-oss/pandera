663beca7e3c19ee727b5ec9538f4188784cb72fa
cosmicBboy <niels.bantilan@gmail.com>
2018-11-13 08:35:35 -0500

    bump version 0.0.4

--- pandera/__init__.py
+++ pandera/__init__.py
@@ -2,4 +2,4 @@ from .pandera import DataFrameSchema, Column, Index, PandasDtype, \
     SeriesSchema, Validator, validate_input, validate_output
 
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pandera",
-    version="0.0.3",
+    version="0.0.4",
     author="Niels Bantilan",
     author_email="niels.bantilan@gmail.com",
     description="A pandas data structure validation library",
