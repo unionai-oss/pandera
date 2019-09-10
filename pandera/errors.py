ed4e8f846d649b6b89dc13d1fc88ba8c3d0bfc2a
Niels Bantilan <niels.bantilan@gmail.com>
2019-09-09 22:08:04 -0400

    add docstrings to error classes (#107)

--- pandera/errors.py
+++ pandera/errors.py
@@ -2,12 +2,15 @@
 
 
 class SchemaInitError(Exception):
+    """Raised when schema initialization fails."""
     pass
 
 
 class SchemaDefinitionError(Exception):
+    """Raised when schema definition is invalid on object validation."""
     pass
 
 
 class SchemaError(Exception):
+    """Raised when object does not pass schema validation constraints."""
     pass
