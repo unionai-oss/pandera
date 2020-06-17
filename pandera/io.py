b1ca7e7392eccb481f9b6ac234d65f69ff244cc0
Niels Bantilan <niels.bantilan@gmail.com>
2020-06-17 16:08:31 -0400

    Update io.py

--- pandera/io.py
+++ pandera/io.py
@@ -156,7 +156,7 @@ def _deserialize_schema(serialized_schema):
             for index_component in serialized_schema["index"]
         ]
 
-    if isinstance(index, type(None)):
+    if index is None:
         index = None
     elif len(index) == 1:
         index = Index(**index[0])
