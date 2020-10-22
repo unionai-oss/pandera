eadcf22732f1c4e15037c88c9d1b3b0195f40fa9
Jean-Francois Zinque <jzinque@gmail.com>
2020-10-22 11:02:44 +0200

    Fix missing typing-related dependencies

--- setup.py
+++ setup.py
@@ -40,7 +40,9 @@ setup(
     install_requires=[
         "numpy >= 1.9.0",
         "pandas >= 0.23.0",
-        "wrapt"
+        "typing_extensions ; python_version<'3.8'",
+        "typing_inspect >= 0.6.0",
+        "wrapt",
     ],
     extras_require=extras_require,
     python_requires='>=3.6',
