c91a2b54174aca9ca8029d5f871cd2ae32c37b5c
Michal Baumgartner <miso.baumgartner@gmail.com>
2020-10-23 10:28:06 +0200

    Add support for Python 3.9

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering'
         ],
 
