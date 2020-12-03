1ff65c9d51e246038850f70a77624c5ca58f0c4f
cosmicBboy <niels.bantilan@gmail.com>
2020-12-03 16:07:34 -0500

    bugfix: check_io allows out=None

--- pandera/decorators.py
+++ pandera/decorators.py
@@ -396,6 +396,8 @@ def check_io(
             out_schemas = [(None, out)]  # type: ignore
         elif isinstance(out, tuple):
             out_schemas = [out]
+        elif out is None:
+            out_schemas = []
         else:
             raise TypeError(
                 f"type of out argument not recognized: {type(out)}"
