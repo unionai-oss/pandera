663b3935bccb99757d95baf5a55163ea5b5afea2
Anton Loukianov <aloukian@unlearn.ai>
2021-05-03 06:20:14 -0700

    Allow attaching registered dataframe checks by using Config field names (#478)
    * enh: allow registering dataframe checks on Schema classes using the Config object
    
    * ci: extend test to show that arguments of checks registered on a child model overwrite the base

--- tests/core/test_model.py
+++ tests/core/test_model.py
@@ -8,6 +8,7 @@ import pandas as pd
 import pytest
 
 import pandera as pa
+import pandera.extensions as pax
 from pandera.typing import DataFrame, Index, Series, String
 
 
@@ -560,6 +561,80 @@ def test_dataframe_check():
         pa.errors.SchemaErrors, match="2 schema errors were found"
     ):
         schema.validate(df, lazy=True)
+
+
+def test_registered_dataframe_checks(
+    extra_registered_checks,
+):  # pylint: disable=unused-argument
+    """Check that custom check inheritance works"""
+    # pylint: disable=unused-variable
+
+    @pax.register_check_method(statistics=["one_arg"])
+    def base_check(df, *, one_arg):
+        # pylint: disable=unused-argument
+        return True
+
+    @pax.register_check_method(statistics=["one_arg", "two_arg"])
+    def child_check(df, *, one_arg, two_arg):
+        # pylint: disable=unused-argument
+        return True
+
+    # pylint: enable=unused-variable
+
+    check_vals = {
+        "one_arg": 150,
+        "two_arg": "hello",
+        "one_arg_prime": "not_150",
+    }
+
+    class Base(pa.SchemaModel):
+        a: Series[int]
+        b: Series[int]
+
+        class Config:
+            no_param_check = ()
+            base_check = check_vals["one_arg"]
+
+    class Child(Base):
+        class Config:
+            base_check = check_vals["one_arg_prime"]
+            child_check = {
+                "one_arg": check_vals["one_arg"],
+                "two_arg": check_vals["two_arg"],
+            }
+
+    base = Base.to_schema()
+    child = Child.to_schema()
+
+    expected_stats_base = {
+        "no_param_check": {},
+        "base_check": {"one_arg": check_vals["one_arg"]},
+    }
+
+    expected_stats_child = {
+        "no_param_check": {},
+        "base_check": {"one_arg": check_vals["one_arg_prime"]},
+        "child_check": {
+            "one_arg": check_vals["one_arg"],
+            "two_arg": check_vals["two_arg"],
+        },
+    }
+
+    assert {b.name: b.statistics for b in base.checks} == expected_stats_base
+    assert {c.name: c.statistics for c in child.checks} == expected_stats_child
+
+    # check that unregistered checks raise
+    with pytest.raises(AttributeError, match=".*custom checks.*"):
+
+        class ErrorSchema(pa.SchemaModel):
+            class Config:
+                unknown_check = {}
+
+        # Check lookup happens at validation/to_schema conversion time
+        # This means that you can register checks after defining a Config,
+        # but also because of caching you can refer to a check that no longer
+        # exists for some order of operations.
+        ErrorSchema.to_schema()
 
 
 def test_config():
