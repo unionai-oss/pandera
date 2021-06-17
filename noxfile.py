7e3db37c8a72e4c0a95a28977ddfca2d911a1aab
cosmicBboy <niels.bantilan@gmail.com>
2021-06-17 09:14:58 -0400

    revert ci

--- noxfile.py
+++ noxfile.py
@@ -289,7 +289,6 @@ def tests(session: Session, extra: str) -> None:
             "--cov-report=term-missing",
             "--cov-report=xml",
             "--cov-append",
-            "-s",
         ]
         if not CI_RUN:
             args.append("--cov-report=html")
