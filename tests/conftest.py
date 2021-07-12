8d7eefbb6b168579cc06aa26ce988310dced88cd
cosmicBboy <niels.bantilan@gmail.com>
2021-07-12 17:06:58 -0400

    loosen strategies unit tests deadline, exclude windows ci

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,6 +16,6 @@ collect_ignore = []
 if not HAS_HYPOTHESIS:
     collect_ignore.append("test_strategies.py")
 else:
-    settings.register_profile("ci", max_examples=100, deadline=5000)
+    settings.register_profile("ci", max_examples=100, deadline=20000)
     settings.register_profile("dev", max_examples=10, deadline=2000)
     settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "dev"))
