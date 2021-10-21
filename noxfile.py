12673aa2ceab37026059ecda6e551b82150da4be
cosmicBboy <niels.bantilan@gmail.com>
2021-10-21 10:51:29 -0400

    wip

--- noxfile.py
+++ noxfile.py
@@ -337,7 +337,6 @@ def tests(session: Session, pandas: str, extra: str) -> None:
             "--cov-report=term-missing",
             "--cov-report=xml",
             "--cov-append",
-            "-vv",
         ]
         if not CI_RUN:
             args.append("--cov-report=html")
