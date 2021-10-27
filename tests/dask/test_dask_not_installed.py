587bd84a0d21cdd4a9070a018d045b82254d0a09
Brian Phillips <brian.phillips@exosfinancial.com>
2021-10-27 08:27:03 -0400

    more coverage improvements

--- tests/dask/test_dask_not_installed.py
+++ tests/dask/test_dask_not_installed.py
@@ -1,11 +1,12 @@
 """Tests behavior when dask is not installed. """
-
 import importlib
+import sys
 from unittest import mock
 
 import pandas as pd
 import pytest
 
+import pandera
 import pandera.check_utils
 import pandera.typing
 
@@ -17,6 +18,10 @@ def test_dask_not_installed() -> None:
         with pytest.raises(ImportError):
             # pylint: disable=reimported,import-outside-toplevel,unused-import
             import dask.dataframe
+
+        del sys.modules["pandera.dask_accessor"]
+        importlib.reload(pandera)
+        assert "pandera.dask_accessor" not in sys.modules
 
         importlib.reload(pandera.check_utils)
         assert not pandera.check_utils.is_table(pd.Series([1]))
