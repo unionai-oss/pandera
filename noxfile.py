b77225c9328cd7ae43025ec8ee37a1cb49a0b224
cosmicBboy <niels.bantilan@gmail.com>
2021-11-10 08:48:13 -0500

    remove ray max version

--- noxfile.py
+++ noxfile.py
@@ -27,7 +27,7 @@ nox.options.sessions = (
 
 DEFAULT_PYTHON = "3.8"
 PYTHON_VERSIONS = ["3.7", "3.8", "3.9"]
-PANDAS_VERSIONS = ["1.2.4", "1.3.0", "latest"]
+PANDAS_VERSIONS = ["1.3.0", "latest"]
 
 PACKAGE = "pandera"
 
