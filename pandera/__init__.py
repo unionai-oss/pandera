931412dd1a1bbd751d83e57bfabff4e1a88618f7
Sean Mackesey <s.mackesey@gmail.com>
2021-11-21 09:37:44 -0800

    add __all__ declaration to root module

--- pandera/__init__.py
+++ pandera/__init__.py
@@ -83,3 +83,77 @@ try:
     from . import modin_accessor
 except ImportError:
     pass
+
+__all__ = [
+    # dtypes
+    "Bool",
+    "Category",
+    "Complex",
+    "Complex64",
+    "Complex128",
+    "Complex256",
+    "DataType",
+    "DateTime",
+    "Float",
+    "Float16",
+    "Float32",
+    "Float64",
+    "Float128",
+    "Int",
+    "Int8",
+    "Int16",
+    "Int32",
+    "Int64",
+    "String",
+    "Timedelta",
+    "Timestamp",
+    "UInt",
+    "UInt8",
+    "UInt16",
+    "UInt32",
+    "UInt64",
+    # numpy_engine
+    "Object",
+    # pandas_engine
+    "BOOL",
+    "INT8",
+    "INT16",
+    "INT32",
+    "INT64",
+    "PANDAS_1_3_0_PLUS",
+    "STRING",
+    "UINT8",
+    "UINT16",
+    "UINT32",
+    "UINT64",
+    # pandera.engines.pandas_engine
+    "PandasDtype",
+    # pandera.engines.pandas_engine
+    "pandas_version",
+    # checks
+    "Check",
+    # decorators
+    "check_input",
+    "check_io",
+    "check_output",
+    "check_types",
+    # hypotheses
+    "Hypothesis",
+    # model
+    "SchemaModel",
+    # model_components
+    "Field",
+    "check",
+    "dataframe_check",
+    # schema_components
+    "Column",
+    "Index",
+    "MultiIndex",
+    # schema_inference
+    "infer_schema",
+    # schemas
+    "DataFrameSchema",
+    "SeriesSchema",
+    # version
+    "__version__",
+]
