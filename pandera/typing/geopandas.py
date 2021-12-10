aa71b2c577bf58519e0b58e8c20ec24f7b5e0f4c
Roshan <roshan@sidewalklabs.com>
2021-12-10 15:36:07 -0500

    fin

--- pandera/typing/geopandas.py
+++ pandera/typing/geopandas.py
@@ -1,6 +1,10 @@
 """Pandera type annotations for GeoPandas."""
+from typing import TYPE_CHECKING, Generic, TypeVar
+
 from pandera.engines.pandas_engine import Geometry
-from pandera.typing.pandas import DataFrame, Series, T
+from pandera.typing.common import DataFrameBase, SeriesBase
+
+from .pandas import Schema
 
 try:
     import geopandas as gpd
@@ -11,8 +15,14 @@ except ImportError:
 
 if GEOPANDAS_INSTALLED:
     # pylint:disable=too-few-public-methods
-    class GeoSeries(Series[Geometry], gpd.GeoSeries):
+    class GeoSeries(SeriesBase[Geometry], gpd.GeoSeries):
         """Representation of geopandas.GeoSeries, only used for type annotation."""
 
-    class GeoDataFrame(DataFrame[T], gpd.GeoDataFrame):
+    # pylint:disable=invalid-name
+    if TYPE_CHECKING:
+        T = TypeVar("T")  # pragma: no cover
+    else:
+        T = Schema
+
+    class GeoDataFrame(DataFrameBase, gpd.GeoDataFrame, Generic[T]):
         """Representation of geopandas.GeoDataFrame, only used for type annotation."""
