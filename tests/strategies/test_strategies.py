05714f26e4f53194ca594db1e3abd1b324a1c3db
cosmicBboy <niels.bantilan@gmail.com>
2022-01-12 00:58:10 -0500

    update strategies tests

--- tests/strategies/test_strategies.py
+++ tests/strategies/test_strategies.py
@@ -33,7 +33,6 @@ UNSUPPORTED_DTYPE_CLS: Set[Any] = set(
         pandas_engine.Interval,
         pandas_engine.Period,
         pandas_engine.Sparse,
-        pandas_engine.Geometry,
     ]
 )
 SUPPORTED_DTYPES = set()
@@ -43,8 +42,13 @@ for data_type in pandas_engine.Engine.get_registered_dtypes():
         getattr(data_type, "bit_width", -1) > 64
         or is_category(data_type)
         or data_type in UNSUPPORTED_DTYPE_CLS
+        or (
+            pandas_engine.GEOPANDAS_INSTALLED
+            and data_type == pandas_engine.Geometry
+        )
     ):
         continue
+
     SUPPORTED_DTYPES.add(pandas_engine.Engine.dtype(data_type))
 
 SUPPORTED_DTYPES.add(pandas_engine.Engine.dtype("datetime64[ns, UTC]"))
