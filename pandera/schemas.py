95ee40d515af9d134c932c069097a5b68bf473c1
jeffzi <jzinque@gmail.com>
2022-05-10 21:53:03 +0200

    fix logical type validation when output is an iterable

--- pandera/schemas.py
+++ pandera/schemas.py
@@ -1981,9 +1981,9 @@ class SeriesSchemaBase:
                     f"expected series '{series.name}' to have type {self._dtype}, "
                     + f"got {series.dtype}"
                 )
-            else:
+            elif not isinstance(check_output, bool):
                 _, failure_cases = check_utils.prepare_series_check_output(
-                    series, check_output
+                    series, pd.Series(check_output)
                 )
                 reshaped_failure_cases = reshape_failure_cases(failure_cases)
                 msg = (
