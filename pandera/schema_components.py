5d856e46142c44fc7515acb46e2be13c92ee8652
Niels Bantilan <niels.bantilan@gmail.com>
2022-09-22 13:51:27 -0400

    Update schema_components.py

--- pandera/schema_components.py
+++ pandera/schema_components.py
@@ -585,19 +585,17 @@ class MultiIndex(DataFrameSchema):
             multiindex_cls = ps.MultiIndex
         return multiindex_cls.from_arrays(
             [
-                # Context 1 : v.dtype may be different than 'object'.
-                # Problem 1 : v.to_numpy()  converts the array dtype to array of 'object' dtype.
-                #          Thus removing the specialized index dtype required to pass a schema's
-                #          index specialized dtype : eg: pandera.typing.Index(pandas.Int64Dtype)
-                # Solution 1: Reuse the original index array to keep the specialized dtype
-                #
-                # Problem 2:  At the time of writing, pyspark v3.3.0 does not provide a working
-                #             implementation of v.array
-                # Solution 2: For Pyspark only, use to_numpy(), with the effect of keeping the
-                #             bug open on this execution environment
-                v.array
-                if "pyspark." not in type(v).__module__
-                else v.to_numpy()
+                # v.dtype may be different than 'object'.
+                # - Reuse the original index array to keep the specialized dtype:
+                #   v.to_numpy()  converts the array dtype to array of 'object' dtype.
+                #   Thus removing the specialized index dtype required to pass a schema's
+                #   index specialized dtype : eg: pandera.typing.Index(pandas.Int64Dtype)
+                # - For Pyspark only, use to_numpy(), with the effect of keeping the
+                #   bug open on this execution environment: At the time of writing, pyspark
+                #   v3.3.0 does not provide a working implementation of v.array
+                v.to_numpy()
+                if type(v).__module__.startswith("pyspark.pandas")
+                else v.array
                 for k, v in sorted(
                     coerced_multi_index.items(), key=lambda x: x[0]
                 )
