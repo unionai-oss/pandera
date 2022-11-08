c1abe8aa5792a24e72d7ea8accfdfbf187c0e297
Arne Recknagel <recknagel@auxmoney.com>
2022-11-08 13:16:42 +0100

    fixed comment
    Signed-off-by: Arne Recknagel <recknagel@auxmoney.com>

--- pandera/errors.py
+++ pandera/errors.py
@@ -166,7 +166,7 @@ class SchemaErrors(ReducedPickleExceptionBase):
             msg += f"- {k}: {v}\n"
 
         def agg_failure_cases(df):
-            # FIXME(arne): hack to support unhashable types, #260 would enable
+            # FIXME(arne): hack to support unhashable types, wait for #260
             df.failure_case = df.failure_case.astype(str)
             # NOTE: this is a hack to add modin support
             if type(df).__module__.startswith("modin.pandas"):
