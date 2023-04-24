ba38028384bf8f1b4dc807abe62b57028b457f70
Neeraj Malhotra <neeraj.malhotra@quantumblack.com>
2023-04-24 15:26:09 -0700

    add new typing for pyspark.sql

--- pandera/typing/__init__.py
+++ pandera/typing/__init__.py
@@ -6,7 +6,7 @@ the typing module.
 
 from typing import Set, Type
 
-from pandera.typing import dask, fastapi, geopandas, modin, pyspark
+from pandera.typing import dask, fastapi, geopandas, modin, pyspark, pyspark_sql
 from pandera.typing.common import (
     BOOL,
     INT8,
@@ -49,14 +49,14 @@ from pandera.typing.common import (
     PYSPARK_DECIMAL,
     PYSPARK_DATE,
     PYSPARK_TIMESTAMP,
-    PYSPARK_BINARY
+    PYSPARK_BINARY,
 )
 from pandera.typing.pandas import DataFrame, Index, Series
-from pandera.typing.pyspark import Column
+from pandera.typing.pyspark_sql import Column
+
 DATAFRAME_TYPES: Set[Type] = {DataFrame}
 SERIES_TYPES: Set[Type] = {Series}
 INDEX_TYPES: Set[Type] = {Index}
-COLUMN_TYPES: Set[Type] = {Column}
 
 if dask.DASK_INSTALLED:
     DATAFRAME_TYPES.update({dask.DataFrame})
@@ -72,17 +72,14 @@ if pyspark.PYSPARK_INSTALLED:
     DATAFRAME_TYPES.update({pyspark.DataFrame})
     SERIES_TYPES.update({pyspark.Series})
     INDEX_TYPES.update({pyspark.Index})  # type: ignore [arg-type]
-    COLUMN_TYPES.update({pyspark.Column})
 
+if pyspark_sql.PYSPARK_SQL_INSTALLED:
+    DATAFRAME_TYPES.update({pyspark_sql.DataFrame})
+    COLUMN_TYPES: Set[Type] = {Column}
 
 if geopandas.GEOPANDAS_INSTALLED:
     DATAFRAME_TYPES.update({geopandas.GeoDataFrame})
     SERIES_TYPES.update({geopandas.GeoSeries})
 
 
-__all__ = [
-    "DataFrame",
-    "Series",
-    "Index",
-    "Column"
-]
+__all__ = ["DataFrame", "Series", "Index", "Column"]
