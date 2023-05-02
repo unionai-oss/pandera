344d37a146b822ece80da0a87e5001e4ce584b9d
jaskaransinghsidana <jaskaran_singh_sidana@mckinsey.com>
2023-05-02 11:36:24 +0530

    Refactoring of the checks test functions

--- tests/pyspark/test_pyspark_check.py
+++ tests/pyspark/test_pyspark_check.py
@@ -1,5 +1,6 @@
 """Unit tests for pyspark container."""
 import datetime
+import random
 from typing import Union
 
 from pyspark.sql import SparkSession
@@ -25,7 +26,7 @@ import pandera.errors
 from pandera.api.pyspark.container import DataFrameSchema
 from pandera.api.pyspark.components import Column
 from pandera.errors import SchemaErrors, PysparkSchemaError
-from tests.pyspark.conftest import spark_df
+from tests.pyspark.conftest import spark_df, sample_check_data
 
 def check_function(spark, check_fn, pass_case_data,
                    fail_case_data, data_types, function_args):
@@ -94,69 +95,133 @@ def test_datatype_check_decorator(spark):
         df = spark.createDataFrame(data=fail_case_data, schema=spark_schema)
         validate_fail_error = fail_schema.report_errors(df)
 
+# {
+#     "Datatype": LongType,
+#     "data": numeric_data
+# },
+#
+#
+# numeric_data = {
+#         "test_pass_data": [("foo", 30), ("bar", 30)],
+#         "test_fail_data": [("foo", 30), ("bar", 31)],
+#         "test_expression": 30,
+#     }
 
-@pytest.mark.parametrize("data_dictionary",  [{"Datatype": LongType,
-                                               "test_pass_data": [("foo", 30), ("bar", 30)],
-                                               "test_fail_data": [("foo", 30), ("bar", 31)],
-                                               "test_expression": 30},
-                                              {"Datatype": IntegerType,
-                                               "test_pass_data": [("foo", 30), ("bar", 30)],
-                                               "test_fail_data": [("foo", 30), ("bar", 31)],
-                                               "test_expression": 30
-                                               },
-                                              {"Datatype": ByteType,
-                                               "test_pass_data": [("foo", 30), ("bar", 30)],
-                                               "test_fail_data": [("foo", 30), ("bar", 31)],
-                                               "test_expression": 30
-                                               },
-                                              {"Datatype": ShortType,
-                                               "test_pass_data": [("foo", 30), ("bar", 30)],
-                                               "test_fail_data": [("foo", 30), ("bar", 31)],
-                                               "test_expression": 30
-                                               },
-                                              {"Datatype": StringType,
-                                               "test_pass_data": [("foo", "a"), ("bar", "a")],
-                                               "test_fail_data": [("foo", "a"), ("bar", "b")],
-                                               "test_expression": "a"
-                                               },
-                                              {"Datatype": DoubleType,
-                                               "test_pass_data": [("foo", 30.0), ("bar", 30.0)],
-                                               "test_fail_data": [("foo", 30.0), ("bar", 31.0)],
-                                               "test_expression": 30.0},
-                                              {"Datatype": TimestampType,
-                                               "test_pass_data": [("foo", datetime.datetime(2020, 10, 1, 10, 0)),
-                                                                  ("bar",  datetime.datetime(2020, 10, 1,  10, 0))],
-                                               "test_fail_data": [("foo",  datetime.datetime(2020, 10, 2,  10, 0)),
-                                                                  ("bar",  datetime.datetime(2020, 10, 2,  10, 0))],
-                                               "test_expression": datetime.datetime(2020, 10, 1,  10, 0)
-                                               },
-                                              {"Datatype": DateType,
-                                               "test_pass_data": [("foo", datetime.date(2020, 10, 1)),
-                                                                  ("bar", datetime.date(2020, 10, 1))],
-                                               "test_fail_data": [("foo", datetime.date(2020, 10, 1)),
-                                                                  ("bar", datetime.date(2020, 10, 2))],
-                                               "test_expression": datetime.date(2020, 10, 1)},
-                                              {"Datatype": DecimalType,
-                                               "test_pass_data": [("foo", decimal.Decimal(30)), ("bar", decimal.Decimal(30))],
-                                               "test_fail_data": [("foo", decimal.Decimal(30)), ("bar", decimal.Decimal(31))],
-                                               "test_expression": decimal.Decimal(30)},
-                                              {"Datatype": FloatType,
-                                               "test_pass_data": [("foo", 30.0), ("bar", 30.0)],
-                                               "test_fail_data": [("foo", 30.0), ("bar", 31.0)],
-                                               "test_expression": 30.0
-                                               },
-                                              {"Datatype": BooleanType,
-                                               "test_pass_data": [("foo", True), ("bar", True)],
-                                               "test_fail_data": [("foo", False), ("bar", False)],
-                                              "test_expression": True}
-                                              ]
-                         )
-@pytest.mark.parametrize("check_fn", [pa.Check.equal_to, pa.Check.eq])
-def test_equal_to_check(spark, check_fn, data_dictionary) -> None:
-    """Test the Check to see if all the values are equal to defined value"""
+class BaseClass:
+    def pytest_generate_tests(self, metafunc):
+        raise NotImplementedError
 
-    check_function(spark, check_fn, data_dictionary['test_pass_data'], data_dictionary['test_fail_data'],
-                   data_dictionary['Datatype'], data_dictionary['test_expression'])
+    @staticmethod
+    def convert_numeric_data(sample_data, convert_type):
+        data_dict = {}
+        for key, value in sample_data.items():
+            if (convert_type=="double") or (convert_type=="float"):
+                if key == "test_expression":
+                    data_dict[key] = float(value)
+                else:
+                    data_dict[key] = [(i[0], float(i[1])) for i in value]
+
+            if convert_type == "decimal":
+                if key == "test_expression":
+                    data_dict[key] = decimal.Decimal(value)
+                else:
+                    data_dict[key] = [(i[0], decimal.Decimal(i[1])) for i in sample_data]
+        return data_dict
+
+    @staticmethod
+    def convert_timestamp_to_date(sample_data):
+        data_dict = {}
+        for key, value in sample_data.items():
+            if key == "test_expression":
+                data_dict[key] = value.date()
+            else:
+                data_dict[key] = [(i[0], (i[1].date())) for i in value]
+
+        return data_dict
+
+class TestEqualToCheck(BaseClass):
+    sample_numeric_data = {"test_pass_data": [("foo", 30), ("bar", 30)],
+                           "test_fail_data": [("foo", 30), ("bar", 31)],
+                           "test_expression": 30}
+    sample_timestamp_data = {"test_pass_data": [("foo", datetime.datetime(2020, 10, 1, 10, 0)),
+                                                ("bar",  datetime.datetime(2020, 10, 1,  10, 0))],
+                             "test_fail_data": [("foo",  datetime.datetime(2020, 10, 2,  10, 0)),
+                                                ("bar",  datetime.datetime(2020, 10, 2,  10, 0))],
+                             "test_expression": datetime.datetime(2020, 10, 1,  10, 0)
+
+                             }
+    def pytest_generate_tests(self, metafunc):
+        # called once per each test function
+        funcarglist = self.get_data_param()[metafunc.function.__name__]
+        argnames = sorted(funcarglist[0])
+        metafunc.parametrize(
+            argnames, [[funcargs[name] for name in argnames] for funcargs in funcarglist]
+        )
+
+    def get_data_param(self):
+       return {"test_equal_to_check": [{"datatype": LongType, "data": self.sample_numeric_data},
+         {"datatype": IntegerType, "data": self.sample_numeric_data},
+         {"datatype": ByteType,  "data": self.sample_numeric_data},
+         {"datatype": ShortType,  "data": self.sample_numeric_data},
+         {"datatype": StringType, "data": {"test_pass_data": [("foo", "a"), ("bar", "a")],
+                       "test_fail_data": [("foo", "a"), ("bar", "b")],
+                       "test_expression": "a"}},
+         {"datatype": DoubleType,"data":
+          self.convert_numeric_data(self.sample_numeric_data, "double")},
+         {"datatype": TimestampType, "data": self.sample_timestamp_data},
+         {"datatype": DateType, "data": self.convert_timestamp_to_date(self.sample_timestamp_data)},
+         {"datatype": DecimalType, "data": self.convert_numeric_data(self.sample_numeric_data, "decimal")},
+         {"datatype": FloatType, "data": self.convert_numeric_data(self.sample_numeric_data, "float")},
+         {"datatype": BooleanType,"data": {
+             "test_pass_data": [("foo", True), ("bar", True)],
+             "test_fail_data": [("foo", False), ("bar", False)],
+             "test_expression": True}}
+         ]}
+
+    @pytest.mark.parametrize("check_fn", [pa.Check.equal_to, pa.Check.eq])
+    def test_equal_to_check(self, spark, check_fn, datatype, data) -> None:
+        """Test the Check to see if all the values are equal to defined value"""
+
+        check_function(spark, check_fn, data['test_pass_data'], data['test_fail_data'],
+                       datatype, data['test_expression'])
+
+#
+# @pytest.mark.parametrize("datatype,data",  [(LongType, sample_check_data),
+#                                             (IntegerType, sample_check_data),
+#                                             (ByteType, sample_check_data),
+#                                             (ShortType, sample_check_data),
+#                                             (StringType, {"test_pass_data": [("foo", "a"), ("bar", "a")],
+#                                                           "test_fail_data": [("foo", "a"), ("bar", "b")],
+#                                                           "test_expression": "a"}),
+#                                             (DoubleType,
+#                                              convert_numeric_data(sample_check_data, "double")),
+#                                             (TimestampType,{
+#                                                "test_pass_data": [("foo", datetime.datetime(2020, 10, 1, 10, 0)),
+#                                                                   ("bar",  datetime.datetime(2020, 10, 1,  10, 0))],
+#                                                "test_fail_data": [("foo",  datetime.datetime(2020, 10, 2,  10, 0)),
+#                                                                   ("bar",  datetime.datetime(2020, 10, 2,  10, 0))],
+#                                                "test_expression": datetime.datetime(2020, 10, 1,  10, 0)
+#                                                }),
+#                                               (DateType,{
+#                                                "test_pass_data": [("foo", datetime.date(2020, 10, 1)),
+#                                                                   ("bar", datetime.date(2020, 10, 1))],
+#                                                "test_fail_data": [("foo", datetime.date(2020, 10, 1)),
+#                                                                   ("bar", datetime.date(2020, 10, 2))],
+#                                                "test_expression": datetime.date(2020, 10, 1)}),
+#                                              (DecimalType, convert_numeric_data(sample_check_data, "decimal")),
+#                                              (FloatType, convert_numeric_data(sample_check_data, "float")),
+#                                              (BooleanType,{
+#                                                "test_pass_data": [("foo", True), ("bar", True)],
+#                                                "test_fail_data": [("foo", False), ("bar", False)],
+#                                               "test_expression": True})
+#                                               ]
+#                          )
+# @pytest.mark.parametrize("check_fn", [pa.Check.equal_to, pa.Check.eq])
+# def test_equal_to_check(spark, check_fn, datatype, data) -> None:
+#     """Test the Check to see if all the values are equal to defined value"""
+#
+#     check_function(spark, check_fn, data['test_pass_data'], data['test_fail_data'],
+#                    datatype, data['test_expression'])
 
 
 
