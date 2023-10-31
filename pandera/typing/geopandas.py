a54d4db01ed44fea5a070290be069fad8353b150
derinwalters <108046357+derinwalters@users.noreply.github.com>
2023-10-31 09:32:07 +0900

    Enhancement to support GeoDataFrame, Geometry coercion, and CRS (Feature/1108) (#1392)
    * Enhance Geometry DataType with coerce method that allows parsing of shapely, wkt, wkb, and geojson dict (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * Proper exception capture (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * pydanticize the GeoDataFrame type and allow for setting crs either via GeoSeries/Geometry or GeoDataFrame (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * implement Geometry CRS annotation and check (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * Removed the Geometry dtype because it was kinda stupid to add in the first place since we only use in Pandas/GeoPandas engine. Add more GeoPandas tests. (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * Because of course I changed a comment and forgot to run formatter before committing (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * documentation comments again (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * Fix issue with multiple-CRS and missing the desired CRS coercion, and add test to cover (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * Add to/from formats to GeoDataFrame to feature-match DataFrame, parameterized Geometry, and a bunch of tests to improve coverage (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * If-else import for typing Annotated (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * switch from dataframe equals to pd.testing.assert_frame_equal due to wonky behavior in python 3.8 (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * black formatter (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * Refactor geopandas tests for clarity, add tests for coverage, and remove the GeoDataFrame from_records due to not understanding its purpose well (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    * pytest.raises exception message match fix that did not come up in nox (#1108)
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>
    
    ---------
    
    Signed-off-by: Derin Walters <derin.c.walters@rijjin.com>

--- pandera/typing/geopandas.py
+++ pandera/typing/geopandas.py
@@ -1,11 +1,39 @@
 """Pandera type annotations for GeoPandas."""
-from typing import TYPE_CHECKING, Generic, TypeVar
+import functools
+import io
+import json
+from typing import (  # type: ignore[attr-defined]
+    TYPE_CHECKING,
+    Any,
+    Generic,
+    TypeVar,
+    Union,
+    get_args,
+    _type_check,
+)
 
-from pandera.typing.common import DataFrameBase, SeriesBase
-from pandera.typing.pandas import DataFrameModel
+import pandas as pd
+
+from pandera.engines import PYDANTIC_V2
+from pandera.errors import SchemaError, SchemaInitError
+from pandera.typing.common import (
+    DataFrameBase,
+    DataFrameModel,
+    SeriesBase,
+)
+from pandera.typing.formats import Formats
+
+if PYDANTIC_V2:
+    from pydantic_core import core_schema
+    from pydantic import GetCoreSchemaHandler
 
 try:
     import geopandas as gpd
+
+    try:
+        from typing import _GenericAlias  # type: ignore[attr-defined]
+    except ImportError:  # pragma: no cover
+        _GenericAlias = None
 
     GEOPANDAS_INSTALLED = True
 except ImportError:  # pragma: no cover
@@ -34,3 +62,196 @@ if GEOPANDAS_INSTALLED:
         """
         A generic type for geopandas.GeoDataFrame.
         """
+
+        if hasattr(gpd.GeoDataFrame, "__class_getitem__") and _GenericAlias:
+
+            def __class_getitem__(cls, item):
+                """Define this to override the patch that pyspark.pandas performs on pandas.
+                https://github.com/apache/spark/blob/master/python/pyspark/pandas/__init__.py#L124-L144
+                """
+                _type_check(item, "Parameters to generic types must be types.")
+                return _GenericAlias(cls, item)
+
+        @classmethod
+        def _coerce_geometry(
+            cls, obj: Union[pd.DataFrame, gpd.GeoDataFrame]
+        ) -> gpd.GeoDataFrame:
+            if not isinstance(obj, gpd.GeoDataFrame):
+                # Construct GeoDataFrame if given a vanilla DataFrame.
+                # We have confidence in geometry column being a Geometry dtype,
+                # so try to coerce and assign as active geometry. Not so much
+                # confidence in other columns because we don't have access to
+                # the schema model here.
+                if "geometry" in obj.columns:
+                    # Coerce data into GeoPandas-acceptible format
+                    obj["geometry"] = Geometry()._coerce_values(
+                        obj["geometry"]
+                    )
+
+                    # Construct with active geometry
+                    obj = gpd.GeoDataFrame(obj, geometry="geometry")
+                else:
+                    # Construct without active geometry
+                    obj = gpd.GeoDataFrame(obj)
+            return obj
+
+        @classmethod
+        def from_format(cls, obj: Any, config) -> gpd.GeoDataFrame:
+            """
+            Converts serialized data from a specific format
+            specified in the :py:class:`pandera.api.pandas.model.DataFrameModel` config options
+            ``from_format`` and ``from_format_kwargs``.
+
+            :param obj: object representing a serialized dataframe.
+            :param config: dataframe model configuration object.
+            """
+            if config.from_format is None:
+                if not isinstance(obj, gpd.GeoDataFrame):
+                    try:
+                        # Start with loading in Pandas because DataFrame
+                        # is more flexible, as opposed to the crotchety
+                        # old man of GeoDataFrame who excepcts its geometry
+                        # column to be ready-made for GeoSeries.
+                        obj = pd.DataFrame(obj)
+
+                        # Coerce into GeoDataFrame, which attempts to
+                        # convert the geometry column even if in a
+                        # non-standard format (geojson dict, wkt, etc.)
+                        obj = cls._coerce_geometry(obj)
+                    except Exception as exc:
+                        raise ValueError(
+                            f"Expected gpd.GeoDataFrame, found {type(obj)}"
+                        ) from exc
+                return obj
+
+            if callable(config.from_format):
+                reader = config.from_format
+            else:
+                reader = {
+                    Formats.dict: gpd.GeoDataFrame,
+                    Formats.csv: pd.read_csv,
+                    Formats.json: lambda x: gpd.GeoDataFrame.from_features(
+                        json.loads(x)
+                    ),
+                    Formats.feather: pd.read_feather,
+                    Formats.parquet: pd.read_parquet,
+                    Formats.pickle: pd.read_pickle,
+                }[Formats(config.from_format)]
+            parsed = reader(obj, **(config.from_format_kwargs or {}))  # type: ignore
+            return cls._coerce_geometry(parsed)
+
+        @classmethod
+        def to_format(cls, data: gpd.GeoDataFrame, config) -> Any:
+            """
+            Converts a geodataframe to the format specified in the
+            :py:class:`pandera.api.pandas.model.DataFrameModel` config options ``to_format``
+            and ``to_format_kwargs``.
+
+            :param data: convert this data to the specified format
+            :param config: :py:cl
+            """
+            if config.to_format is None:
+                return data
+
+            if callable(config.to_format):
+                writer = functools.partial(config.to_format, data)
+                if callable(config.to_format_buffer):
+                    buffer = config.to_format_buffer()
+                elif config.to_format_buffer is None:
+                    buffer = None
+                else:  # pragma: no cover
+                    raise TypeError(
+                        "to_format_buffer must be Callable or None, found "
+                        f"{config.to_format_buffer}"
+                    )
+            else:
+                writer, buffer = {  # type: ignore[assignment]
+                    Formats.dict: (data.to_dict, None),
+                    Formats.csv: (data.to_csv, None),
+                    Formats.json: (data.to_json, None),
+                    Formats.feather: (data.to_feather, io.BytesIO()),
+                    Formats.parquet: (data.to_parquet, io.BytesIO()),
+                    Formats.pickle: (data.to_pickle, io.BytesIO()),
+                }[Formats(config.to_format)]
+
+            args = [] if buffer is None else [buffer]
+            out = writer(*args, **(config.to_format_kwargs or {}))  # type: ignore
+            if buffer is None:
+                return out
+            elif buffer.closed:
+                raise IOError(
+                    f"geopandas=={gpd.__version__} closed the buffer automatically "
+                    f"using the serialization method {writer}. Use a later "
+                    "version of pandas or use a different the serialization "
+                    "format."
+                )
+            buffer.seek(0)
+            return buffer
+
+        @classmethod
+        def _get_schema_model(cls, field):
+            if not field.sub_fields:
+                raise TypeError(
+                    "Expected a typed pandera.typing.GeoDataFrame,"
+                    " e.g. GeoDataFrame[Schema]"
+                )
+            schema_model = field.sub_fields[0].type_
+            return schema_model
+
+        if PYDANTIC_V2:
+
+            @classmethod
+            def __get_pydantic_core_schema__(
+                cls, _source_type: Any, _handler: GetCoreSchemaHandler
+            ) -> core_schema.CoreSchema:
+                schema_model = get_args(_source_type)[0]
+                return core_schema.no_info_plain_validator_function(
+                    functools.partial(
+                        cls.pydantic_validate,
+                        schema_model=schema_model,
+                    ),
+                )
+
+        else:
+
+            @classmethod
+            def __get_validators__(cls):
+                yield cls._pydantic_validate
+
+        @classmethod
+        def pydantic_validate(cls, obj: Any, schema_model) -> gpd.GeoDataFrame:
+            """
+            Verify that the input can be converted into a pandas dataframe that
+            meets all schema requirements.
+
+            This is for pydantic >= v2
+            """
+            try:
+                schema = schema_model.to_schema()
+            except SchemaInitError as exc:
+                raise ValueError(
+                    f"Cannot use {cls.__name__} as a pydantic type as its "
+                    "DataFrameModel cannot be converted to a DataFrameSchema.\n"
+                    f"Please revisit the model to address the following errors:"
+                    f"\n{exc}"
+                ) from exc
+
+            data = cls.from_format(obj, schema_model.__config__)
+
+            try:
+                valid_data = schema.validate(data)
+            except SchemaError as exc:
+                raise ValueError(str(exc)) from exc
+
+            return cls.to_format(valid_data, schema_model.__config__)
+
+        @classmethod
+        def _pydantic_validate(cls, obj: Any, field) -> gpd.GeoDataFrame:
+            """
+            Verify that the input can be converted into a geopandas geodataframe that
+            meets all schema requirements.
+
+            This is for pydantic < v1
+            """
+            schema_model = cls._get_schema_model(field)
+            return cls.pydantic_validate(obj, schema_model)
--- tests/geopandas/test_geopandas.py
+++ tests/geopandas/test_geopandas.py
@@ -1,12 +1,19 @@
 """Unit tests for the geopandas integration."""
 
+try:  # python 3.9+
+    from typing import Annotated  # type: ignore
+except ImportError:
+    from typing_extensions import Annotated  # type: ignore
+
+import pandas as pd
 import geopandas as gpd
 import pytest
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, Point
 
 import pandera as pa
 from pandera.typing import Series
 from pandera.typing.geopandas import GeoDataFrame, GeoSeries
+from pandera.engines.pandas_engine import Geometry
 
 
 def test_dataframe_schema():
@@ -31,12 +38,12 @@ def test_dataframe_schema():
 @pytest.mark.parametrize(
     "data,invalid",
     [
+        [["what the heck?!"], True],
         [[Polygon(((0, 0), (0, 1), (1, 1), (1, 0)))] * 2, True],
         [[Polygon(((0, 0), (0, -2), (-2, -2), (-2, 0)))] * 2, False],
     ],
 )
 def test_schema_model(data, invalid: bool):
-    # pylint: disable=missing-class-docstring
     """Test that DataFrameModel works on GeoDataFrames."""
 
     class Schema(pa.DataFrameModel):
@@ -57,4 +64,240 @@ def test_schema_model(data, invalid: bool):
 
     assert isinstance(
         GeoDataFrame[Schema]({"geometry": data}), gpd.GeoDataFrame
+    )
+
+
+@pytest.mark.parametrize(
+    "gdf_args,invalid",
+    [
+        [
+            {
+                "geometry": [Polygon(((0, 0), (0, -2), (-2, -2), (-2, 0)))]
+                * 2,
+                "crs": None,
+            },
+            True,
+        ],
+        [
+            {
+                "geometry": [Polygon(((0, 0), (0, -2), (-2, -2), (-2, 0)))]
+                * 2,
+                "crs": "EPSG:4326",
+            },
+            False,
+        ],
+        [
+            {
+                "geometry": [Polygon(((0, 0), (0, -2), (-2, -2), (-2, 0)))]
+                * 2,
+                "crs": "EPSG:25832",
+            },
+            True,
+        ],
+    ],
+)
+def test_schema_dtype_crs_without_coerce(gdf_args, invalid: bool):
+    """Test Geometry crs annotation without coerce."""
+    # No CRS to validate
+    class Schema(pa.DataFrameModel):
+        # pylint: disable=missing-class-docstring
+        geometry: Geometry(crs="EPSG:4326")  # type: ignore
+
+    # create a geodataframe that's validated on object initialization
+    if invalid:
+        with pytest.raises(TypeError):
+            GeoDataFrame[Schema](**gdf_args)
+        return
+
+    assert isinstance(GeoDataFrame[Schema](**gdf_args), gpd.GeoDataFrame)
+
+
+@pytest.mark.parametrize(
+    "gdf_args,invalid",
+    [
+        [
+            {
+                "geometry": [Polygon(((0, 0), (0, -2), (-2, -2), (-2, 0)))]
+                * 2,
+                "crs": None,
+            },
+            False,
+        ],
+        [
+            {
+                "geometry": [Polygon(((0, 0), (0, -2), (-2, -2), (-2, 0)))]
+                * 2,
+                "crs": "EPSG:4326",
+            },
+            False,
+        ],
+        [
+            {
+                "geometry": [Polygon(((0, 0), (0, -2), (-2, -2), (-2, 0)))]
+                * 2,
+                "crs": "EPSG:25832",
+            },
+            False,
+        ],
+    ],
+)
+def test_schema_dtype_crs_with_coerce(gdf_args, invalid: bool):
+    """Test Geometry crs annotation with coerce."""
+    # No CRS to validate
+    class Schema(pa.DataFrameModel):
+        # pylint: disable=missing-class-docstring
+        geometry: Geometry(crs="EPSG:4326")  # type: ignore
+
+        class Config:
+            coerce = True
+
+    # create a geodataframe that's validated on object initialization
+    if invalid:
+        with pytest.raises(TypeError):
+            GeoDataFrame[Schema](**gdf_args)
+        return
+
+    gdf = GeoDataFrame[Schema](**gdf_args)
+    assert isinstance(gdf, gpd.GeoDataFrame)
+    assert gdf.crs == "EPSG:4326"
+
+
+def test_schema_parametrized_crs():
+    """Test Geometry declaration using dtype_kwargs and Annotated."""
+
+    gdf = gpd.GeoDataFrame({"geometry": [Point([1, 1])]}, crs="EPSG:4326")
+
+    class Schema1(pa.DataFrameModel):
+        # pylint: disable=missing-class-docstring
+        geometry: Geometry = pa.Field(dtype_kwargs={"crs": "EPSG:4326"})
+
+    assert isinstance(GeoDataFrame[Schema1](gdf), gpd.GeoDataFrame)
+
+    class Schema2(pa.DataFrameModel):
+        # pylint: disable=missing-class-docstring
+        geometry: Annotated[GeoSeries, "EPSG:4326"]
+
+    assert isinstance(GeoDataFrame[Schema2](gdf), gpd.GeoDataFrame)
+
+
+def test_schema_multiple_geometry_same_crs():
+    """Test GeoDataFrame with multiple GeoSeries columns on same CRS"""
+
+    class Schema(pa.DataFrameModel):
+        # pylint: disable=missing-class-docstring
+        geometry: Geometry(crs="EPSG:4326")
+        random: Geometry(crs="EPSG:4326")
+
+    data = {
+        "geometry": gpd.GeoSeries(
+            [Point([1, 1])], name="geometry", crs="EPSG:4326"
+        ),
+        "random": gpd.GeoSeries(
+            [Point([2, 2])], name="random", crs="EPSG:4326"
+        ),
+    }
+
+    # Both columns should have same CRS
+    gdf = GeoDataFrame[Schema](data)
+    pd.testing.assert_series_equal(gdf["geometry"], data["geometry"])
+    pd.testing.assert_series_equal(gdf["random"], data["random"])
+
+
+def test_schema_multiple_geometry_different_crs():
+    """Test GeoDataFrame with multiple GeoSeries columns on different CRS"""
+
+    class Schema(pa.DataFrameModel):
+        # pylint: disable=missing-class-docstring
+        geometry: Geometry(crs="EPSG:4326")
+        random: Geometry(crs="EPSG:3857")
+
+        class Config:
+            # pylint: disable=missing-class-docstring
+            coerce = True
+
+    data = {"geometry": [Point([1, 1])], "random": [Point([450000, 900000])]}
+
+    # geometry is assigned EPSG:3857 by gpd.GeoDataFrame constructor,
+    # while random isn't assigned anything. Post-coercion, both are
+    # converted to their respective CRS schema.
+    gdf = GeoDataFrame[Schema](data, crs="EPSG:3857")
+    assert gdf.geometry.crs == "EPSG:4326"
+    assert gdf["random"].crs == "EPSG:3857"
+
+    # geometry is assigned EPSG:3395 by gpd.GeoDataFrame.to_crs, while random
+    # is left unchanged
+    gdf = gdf.to_crs("EPSG:3395")
+    assert gdf.geometry.crs == "EPSG:3395"
+    assert gdf["random"].crs == "EPSG:3857"
+
+    # Pandera coerces geometry back to schema
+    gdf = GeoDataFrame[Schema](data)
+    assert gdf.geometry.crs == "EPSG:4326"
+    assert gdf["random"].crs == "EPSG:3857"
+
+    # random is assigned EPSG:4326 by gpd.GeoDataFrame.to_crs, while
+    # geometry is left unchanged
+    gdf["random"] = gdf["random"].to_crs("EPSG:4326")
+    assert gdf.geometry.crs == "EPSG:4326"
+    assert gdf["random"].crs == "EPSG:4326"
+
+    # Pandera coerces random back to schema
+    gdf = GeoDataFrame[Schema](data)
+    assert gdf.geometry.crs == "EPSG:4326"
+    assert gdf["random"].crs == "EPSG:3857"
+
+
+@pytest.mark.parametrize(
+    "data,invalid",
+    [
+        [
+            pd.DataFrame(
+                {"geometry": [Polygon(((0, 0), (0, 1), (1, 1), (1, 0)))] * 2},
+            ),
+            False,
+        ],
+        [
+            pd.DataFrame(
+                {"geometry": ["a", "b"]},
+            ),
+            True,
+        ],
+        [
+            gpd.GeoDataFrame(
+                {"geometry": [Polygon(((0, 0), (0, 1), (1, 1), (1, 0)))] * 2},
+                crs="EPSG:25832",
+            ),
+            False,
+        ],
+    ],
+)
+def test_schema_from_dataframe(data, invalid: bool):
+    """Test that DataFrameModel works on gpd.GeoDataFrame or pd.DataFrame input."""
+
+    class Schema(pa.DataFrameModel):
+        # pylint: disable=missing-class-docstring
+        geometry: GeoSeries
+
+        class Config:
+            coerce = True
+
+    # create a geodataframe that's validated on object initialization
+    if invalid:
+        with pytest.raises(pa.errors.SchemaError):
+            GeoDataFrame[Schema](data)
+        return
+
+    assert isinstance(GeoDataFrame[Schema](data), gpd.GeoDataFrame)
+
+
+def test_schema_no_geometry():
+    """Test that GeoDataFrame can be constructed from data without a Geometry column."""
+
+    class Schema(pa.DataFrameModel):
+        # pylint: disable=missing-class-docstring
+        name: str
+
+    # create a geodataframe that's validated on object initialization
+    assert isinstance(
+        GeoDataFrame[Schema]({"name": ["a", "b"]}), gpd.GeoDataFrame
     )
