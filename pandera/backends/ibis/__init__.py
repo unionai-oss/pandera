a277f5e95297203964439d9b44d4f5f40e07656b
Deepyaman Datta <deepyaman.datta@utexas.edu>
2023-12-20 10:15:48 -0700

    Add code for basic `Column`, and stub more modules
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    Signed-off-by: Andrej Iring <airing@powerfulmedical.com>

---
+++ pandera/backends/ibis/__init__.py
@@ -0,0 +1,5 @@
+"""Ibis backend implementation for schemas and checks."""
+
+from pandera.api.checks import Check
+from pandera.api.ibis.container import DataFrameSchema
+from pandera.api.ibis.components import Column
