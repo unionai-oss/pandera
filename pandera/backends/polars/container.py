bb9ee314107e7ed6a4db0dde137d1334c6458f2f
cosmicBboy <niels.bantilan@gmail.com>
2024-02-24 10:32:07 -0500

    implement methods for polars backend
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- pandera/backends/polars/container.py
+++ pandera/backends/polars/container.py
@@ -1,11 +1,11 @@
 """Validation backend for polars DataFrameSchema."""
 
-from typing import Optional, List
+from typing import Any, Optional, List, Callable, Tuple
 
 import polars as pl
 
 from pandera.api.polars.container import DataFrameSchema
-from pandera.backends.base import BaseSchemaBackend, CoreCheckResult
+from pandera.backends.base import CoreCheckResult
 from pandera.backends.polars.base import PolarsSchemaBackend
 from pandera.error_handlers import SchemaErrorHandler
 from pandera.errors import (
@@ -29,10 +29,31 @@ class DataFrameSchemaBackend(PolarsSchemaBackend):
         inplace: bool = False,
     ):
         error_handler = SchemaErrorHandler(lazy)
-        components = [v for k, v in schema.columns.items()]
+
+        column_info = self.collect_column_info(check_obj, schema)  # TODO
+
+        core_parsers: List[Tuple[Callable[..., Any], Tuple[Any, ...]]] = [
+            ((self.add_missing_columns), (schema, column_info)),  # TODO
+            (self.strict_filter_columns, (schema, column_info)),  # TODO
+            (self.coerce_dtype, (schema,)),  # TODO
+        ]
+
+        for parser, args in core_parsers:
+            try:
+                check_obj = parser(check_obj, *args)
+            except SchemaError as exc:
+                error_handler.collect_error(exc.reason_code, exc)
+            except SchemaErrors as exc:
+                error_handler.collect_errors(exc)
+
+        components = [v for _, v in schema.columns.items()]
 
         core_checks = [
+            # (self.check_column_names_are_unique, (check_obj, schema)),  # TODO
+            # (self.check_column_presence, (check_obj, schema, column_info)),  # TODO
+            # (self.check_column_values_are_unique, (sample, schema)),  # TODO
             (self.run_schema_component_checks, (check_obj, components, lazy))
+            # (self.run_checks, (sample, schema)),  # TODO
         ]
 
         for check, args in core_checks:
@@ -40,6 +61,7 @@ class DataFrameSchemaBackend(PolarsSchemaBackend):
             if isinstance(results, CoreCheckResult):
                 results = [results]
 
+            # pylint: disable=no-member
             for result in results:
                 if result.passed:
                     continue
@@ -111,3 +133,62 @@ class DataFrameSchemaBackend(PolarsSchemaBackend):
                 )
         assert all(check_passed)
         return check_results
+
+    def collect_column_info(self, check_obj: pl.LazyFrame, schema):
+        """Collect column metadata for the dataframe."""
+        raise NotImplementedError
+
+    ###########
+    # Parsers #
+    ###########
+
+    def add_missing_columns(
+        self,
+        check_obj: pl.LazyFrame,
+        schema,
+        column_info: Any,
+    ):
+        """Add missing columns to the dataframe."""
+        raise NotImplementedError
+
+    def strict_filter_columns(
+        self,
+        check_obj: pl.LazyFrame,
+        schema,
+        column_info: Any,
+    ):
+        """Filter columns based on schema."""
+        raise NotImplementedError
+
+    def coerce_dtype(self, check_obj: pl.LazyFrame, schema=None):
+        """Coerce dataframe columns to the correct dtype."""
+        raise NotImplementedError
+
+    ##########
+    # Checks #
+    ##########
+
+    def check_column_names_are_unique(
+        self,
+        check_obj: pl.LazyFrame,
+        schema,
+    ) -> CoreCheckResult:
+        """Check that column names are unique."""
+        raise NotImplementedError
+
+    def check_column_presence(
+        self,
+        check_obj: pl.LazyFrame,
+        schema,
+        column_info: Any,
+    ) -> List[CoreCheckResult]:
+        """Check that all columns in the schema are present in the dataframe."""
+        raise NotImplementedError
+
+    def check_column_values_are_unique(
+        self,
+        check_obj: pl.LazyFrame,
+        schema,
+    ) -> CoreCheckResult:
+        """Check that column values are unique."""
+        raise NotImplementedError
