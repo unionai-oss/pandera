758f730434f62d2c82d5bb4e2c9e3b09005ac5a8
Baden Ashford <baden.ashford@gmail.com>
2024-02-29 19:31:39 +1100

    Update assertions
    Signed-off-by: Baden Ashford <baden.ashford@gmail.com>

--- tests/core/test_schema_components.py
+++ tests/core/test_schema_components.py
@@ -263,10 +263,10 @@ def tests_multi_index_subindex_coerce() -> None:
 
     # coerce=False at the MultiIndex level should result in two type errors
     schema = DataFrameSchema(index=MultiIndex(indexes))
-    with pytest.raises(
-        errors.SchemaErrors, match="A total of 2 schema errors were found"
-    ):
+    with pytest.raises(errors.SchemaErrors) as e:
         schema(data, lazy=True)
+
+    assert len(e.value.message["SCHEMA"]) == 1
 
 
 @pytest.mark.skipif(
@@ -297,9 +297,10 @@ def tests_multi_index_subindex_coerce_with_empty_subindex(coerce) -> None:
     else:
         with pytest.raises(
             errors.SchemaErrors,
-            match=r"A total of \d+ schema errors were found",
-        ):
+        ) as e:
             schema_override(data, lazy=True)
+
+        assert len(e.value.message["SCHEMA"]) == 1
 
 
 def test_schema_component_equality_operators():
