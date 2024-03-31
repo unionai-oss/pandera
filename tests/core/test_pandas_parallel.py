522db17f7b4a2a87bea2dca75d5af73f04470788
cosmicBboy <niels.bantilan@gmail.com>
2024-03-30 21:19:59 -0400

    fix pandas unit test
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- tests/core/test_pandas_parallel.py
+++ tests/core/test_pandas_parallel.py
@@ -7,10 +7,10 @@ from pandera import Column, DataFrameSchema
 
 def test_polars_parallel():
     def fn():
-        schema = DataFrameSchema({"a": Column(int)}, coerce=True)
+        schema = DataFrameSchema({"a": Column("int64")}, coerce=True)
         return schema.validate(pd.DataFrame({"a": [1]}))
 
     results = Parallel(2)([delayed(fn)() for _ in range(10)])
     assert len(results) == 10
     for result in results:
-        assert result.dtypes["a"] == int
+        assert result.dtypes["a"] == "int64"
