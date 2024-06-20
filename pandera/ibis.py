4b4827b2d607f31f44c740b2592781e7938b01db
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-06-20 07:58:33 -0600

    implement column, add basic unit tests for Ibis data types
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- pandera/ibis.py
+++ pandera/ibis.py
@@ -5,3 +5,4 @@ import pandera.backends.ibis
 from pandera.api.checks import Check
 from pandera.api.ibis.components import Column
 from pandera.api.ibis.container import DataFrameSchema
+from pandera.api.ibis.model import DataFrameModel
