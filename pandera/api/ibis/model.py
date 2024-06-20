b6bda12a595cfe596904cc669283df90bb90f1bd
Deepyaman Datta <deepyaman.datta@utexas.edu>
2024-06-20 07:58:33 -0600

    implement column, add basic unit tests for Ibis data types
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- pandera/api/ibis/model.py
+++ pandera/api/ibis/model.py
@@ -1,9 +1,20 @@
 """Class-based API for Ibis models."""
 
+import inspect
+from typing import Dict, List, Tuple
+
+import ibis.expr.datatypes as dt
 import ibis.expr.types as ir
 
+from pandera.api.checks import Check
 from pandera.api.dataframe.model import DataFrameModel as _DataFrameModel
+from pandera.api.dataframe.model import get_dtype_kwargs
+from pandera.api.dataframe.model_components import FieldInfo
+from pandera.api.ibis.components import Column
 from pandera.api.ibis.container import DataFrameSchema
+from pandera.engines import ibis_engine
+from pandera.errors import SchemaInitError
+from pandera.typing import AnnotationInfo
 
 
 class DataFrameModel(_DataFrameModel[ir.Table, DataFrameSchema]):
@@ -13,3 +24,81 @@ class DataFrameModel(_DataFrameModel[ir.Table, DataFrameSchema]):
 
     See the :ref:`User Guide <dataframe-models>` for more.
     """
+
+    @classmethod
+    def build_schema_(cls, **kwargs) -> DataFrameSchema:
+        return DataFrameSchema(
+            cls._build_columns(cls.__fields__, cls.__checks__),
+            checks=cls.__root_checks__,
+            **kwargs,
+        )
+
+    @classmethod
+    def _build_columns(  # pylint:disable=too-many-locals
+        cls,
+        fields: Dict[str, Tuple[AnnotationInfo, FieldInfo]],
+        checks: Dict[str, List[Check]],
+    ) -> Dict[str, Column]:
+        columns: Dict[str, Column] = {}
+        for field_name, (annotation, field) in fields.items():
+            field_checks = checks.get(field_name, [])
+            field_name = field.name
+            check_name = getattr(field, "check_name", None)
+
+            try:
+                engine_dtype = ibis_engine.Engine.dtype(
+                    annotation.raw_annotation
+                )
+                if inspect.isclass(annotation.raw_annotation) and issubclass(
+                    annotation.raw_annotation, ibis_engine.DataType
+                ):
+                    # use the raw annotation as the dtype if it's a native
+                    # pandera Ibis datatype
+                    dtype = annotation.raw_annotation
+                else:
+                    dtype = engine_dtype.type
+            except (TypeError, ValueError) as exc:
+                if annotation.metadata:
+                    if field.dtype_kwargs:
+                        raise TypeError(
+                            "Cannot specify redundant 'dtype_kwargs' "
+                            + f"for {annotation.raw_annotation}."
+                            + "\n Usage Tip: Drop 'typing.Annotated'."
+                        ) from exc
+                    dtype_kwargs = get_dtype_kwargs(annotation)
+                    dtype = annotation.arg(**dtype_kwargs)  # type: ignore
+                elif annotation.default_dtype:
+                    dtype = annotation.default_dtype
+                else:
+                    dtype = annotation.arg  # type: ignore
+
+            if (
+                annotation.origin is None
+                or isinstance(annotation.origin, dt.DataType)
+                # or annotation.origin is Series  # TODO(deepyaman): Implement `Series`.
+                or dtype
+            ):
+                if check_name is False:
+                    raise SchemaInitError(
+                        f"'check_name' is not supported for {field_name}."
+                    )
+
+                column_kwargs = (
+                    field.column_properties(
+                        dtype,
+                        required=not annotation.optional,
+                        checks=field_checks,
+                        name=field_name,
+                    )
+                    if field
+                    else {}
+                )
+                columns[field_name] = Column(**column_kwargs)
+
+            else:
+                raise SchemaInitError(
+                    f"Invalid annotation '{field_name}: "
+                    f"{annotation.raw_annotation}'."
+                )
+
+        return columns
---
+++ tests/ibis/test_ibis_container.py
@@ -0,0 +1,47 @@
+"""Unit tests for Ibis container."""
+
+import ibis
+import ibis.expr.datatypes as dt
+import ibis.expr.types as ir
+import pandas as pd
+import pytest
+
+import pandera as pa
+from pandera.ibis import Column, DataFrameSchema
+
+
+@pytest.fixture
+def t_basic():
+    """Basic Ibis table fixture."""
+    df = pd.DataFrame(
+        {
+            "string_col": ["0", "1", "2"],
+            "int_col": [0, 1, 2],
+        }
+    )
+    return ibis.memtable(df, name="t")
+
+
+@pytest.fixture
+def t_schema_basic():
+    """Basic Ibis table schema fixture."""
+    return DataFrameSchema(
+        {
+            # "string_col": Column(str),
+            "int_col": Column(dt.Int64),
+        }
+    )
+
+
+def test_basic_ibis_table(t_basic, t_schema_basic):
+    """Test basic Ibis table."""
+    query = t_schema_basic.validate(t_basic)
+    assert isinstance(query, ir.Table)
+
+
+def test_basic_ibis_table_dtype_error(t_basic, t_schema_basic):
+    """Test basic Ibis table."""
+    t = t_basic.mutate(int_col=t_basic.int_col.cast("int32"))
+    with pytest.raises(pa.errors.SchemaError):
+        # type check errors occur even before collection
+        t_schema_basic.validate(t)
