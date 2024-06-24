f61be84e5bba67d4d5c9203a9a4d6aaa42c5aa2a
Alp Aribal <alparbal@gmail.com>
2024-06-24 21:38:25 +0100

    move self.type into init
    Signed-off-by: Alp Aribal <alparbal@gmail.com>

--- pandera/engines/pandas_engine.py
+++ pandera/engines/pandas_engine.py
@@ -1297,11 +1297,11 @@ if GEOPANDAS_INSTALLED:
 class PydanticModel(DataType):
     """A pydantic model datatype applying to rows in a dataframe."""
 
-    type: Type[BaseModel] = dataclasses.field(default=None, init=False)  # type: ignore # noqa
     auto_coerce = True
 
     # pylint:disable=super-init-not-called
     def __init__(self, model: Type[BaseModel]) -> None:
+        self.type: Type[BaseModel]
         object.__setattr__(self, "type", model)
 
     def coerce(self, data_container: PandasObject) -> PandasObject:
