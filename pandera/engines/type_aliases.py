9b30a8ef81ab32966ccfcc5fa233d72f24904e6c
Niels Bantilan <niels.bantilan@gmail.com>
2024-07-17 15:48:23 -0400

    handle case when pandera is run with optimized python mode (#1749)
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- pandera/engines/type_aliases.py
+++ pandera/engines/type_aliases.py
@@ -5,16 +5,7 @@ from typing import Union
 import numpy as np
 import pandas as pd
 
-try:
-    from pyspark.sql import DataFrame
-
-    PYSPARK_INSTALLED = True
-except ImportError:  # pragma: no cover
-    PYSPARK_INSTALLED = False
 
 PandasObject = Union[pd.Series, pd.DataFrame]
 PandasExtensionType = pd.core.dtypes.base.ExtensionDtype
 PandasDataType = Union[pd.core.dtypes.base.ExtensionDtype, np.dtype, type]
-
-if PYSPARK_INSTALLED:
-    PysparkObject = Union[DataFrame]
