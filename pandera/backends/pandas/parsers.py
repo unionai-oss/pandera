a5f52fb482ea27c4dd5e3e445924cecf392e6ace
cosmicBboy <niels.bantilan@gmail.com>
2024-09-22 21:33:39 -0400

    revert changes made in #1803 due to import loading time performance hit
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- pandera/backends/pandas/parsers.py
+++ pandera/backends/pandas/parsers.py
@@ -4,10 +4,10 @@ from functools import partial
 from typing import Dict, Optional, Union
 
 import pandas as pd
-from multimethod import multidispatch
+from multimethod import overload
 
 from pandera.api.base.parsers import ParserResult
-from pandera.api.pandas.types import Field, Table
+from pandera.api.pandas.types import is_field, is_table
 from pandera.api.parsers import Parser
 from pandera.backends.base import BaseParserBackend
 
@@ -22,40 +22,40 @@ class PandasParserBackend(BaseParserBackend):
         self.parser = parser
         self.parser_fn = partial(parser._parser_fn, **parser._parser_kwargs)
 
-    @multidispatch
+    @overload
     def preprocess(
         self, parse_obj, key  # pylint:disable=unused-argument
     ) -> pd.Series:  # pylint:disable=unused-argument
         """Preprocesses a parser object before applying the parse function."""
         return parse_obj
 
-    @preprocess.register
-    def _(
+    @overload  # type: ignore [no-redef]
+    def preprocess(
         self,
-        parse_obj: Table,  # type: ignore [valid-type]
+        parse_obj: is_table,  # type: ignore [valid-type]
         key,
     ) -> Union[pd.DataFrame, Dict[str, pd.DataFrame]]:
         return parse_obj[key]
 
-    @preprocess.register
-    def _(
-        self, parse_obj: Table, key: None  # type: ignore [valid-type]  # pylint:disable=unused-argument
+    @overload  # type: ignore [no-redef]
+    def preprocess(
+        self, parse_obj: is_table, key: None  # type: ignore [valid-type]  # pylint:disable=unused-argument
     ) -> Union[pd.DataFrame, Dict[str, pd.DataFrame]]:
         return parse_obj
 
-    @multidispatch
+    @overload
     def apply(self, parse_obj):
         """Apply the parse function to a parser object."""
         raise NotImplementedError
 
-    @apply.register
-    def _(self, parse_obj: Field):  # type: ignore [valid-type]
+    @overload  # type: ignore [no-redef]
+    def apply(self, parse_obj: is_field):  # type: ignore [valid-type]
         if self.parser.element_wise:
             return parse_obj.map(self.parser_fn)
         return self.parser_fn(parse_obj)
 
-    @apply.register
-    def _(self, parse_obj: Table):  # type: ignore [valid-type]
+    @overload  # type: ignore [no-redef]
+    def apply(self, parse_obj: is_table):  # type: ignore [valid-type]
         if self.parser.element_wise:
             return getattr(parse_obj, "map", parse_obj.applymap)(
                 self.parser_fn
