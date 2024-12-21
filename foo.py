2783d2edea4e0073ea3010e074e9af7cda457257
cosmicBboy <niels.bantilan@gmail.com>
2024-12-21 17:47:02 -0500

    remove perf.py script
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- foo.py
+++
@@ -1,8 +0,0 @@
-import pandera as pa
-import pandas as pd
-
-schema = pa.DataFrameSchema(
-    columns={"a": pa.Column(int, pa.Check.greater_than(0))},
-)
-
-schema.validate(pd.DataFrame({"a": [1, 2, 3]}))
