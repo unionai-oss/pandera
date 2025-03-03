e0c9212073325b7857c391629b04395eae903b81
cosmicBboy <niels.bantilan@gmail.com>
2025-03-02 22:06:09 -0500

    move minumum version enforcement for pandas and numpy
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- pandera/__init__.py
+++ pandera/__init__.py
@@ -2,8 +2,6 @@
 """A flexible and expressive dataframe validation library."""
 
 import sys
-import warnings
-from packaging.version import parse
 
 
 try:
@@ -18,29 +16,5 @@ else:
 if not _pandas_installed:
     sys.exit(0)
 
-
-_min_pandas_version = parse("2.1.1")
-_min_numpy_version = parse("1.24.4")
-
-
-if parse(pd.__version__) < _min_pandas_version:
-    raise ImportError(
-        "pandera requires pandas >= 2.1.1, but you have pandas "
-        f"{pd.__version__}. Please upgrade pandas to the minimum supported version."
-    )
-
-if parse(np.__version__) < _min_numpy_version:
-    raise ImportError(
-        "pandera requires numpy >= 1.24.4, but you have numpy "
-        f"{np.__version__}. Please upgrade numpy to the minimum supported version."
-    )
-
-
-warnings.warn(
-    "The pandera module for pandas data validation has been moved to "
-    "`pandera.pandas`. Please update your import statement to "
-    "`import pandera.pandas as pa`.",
-    FutureWarning,
-)
 
 from pandera.pandas import *
--- pandera/pandas.py
+++ pandera/pandas.py
@@ -2,9 +2,39 @@
 """A flexible and expressive pandas dataframevalidation library."""
 
 import platform
+import warnings
+
+from packaging.version import parse
+
+import pandas as pd
+import numpy as np
+
+_min_pandas_version = parse("2.1.1")
+_min_numpy_version = parse("1.24.4")
+
+
+if parse(pd.__version__) < _min_pandas_version:
+    raise ImportError(
+        "pandera requires pandas >= 2.1.1, but you have pandas "
+        f"{pd.__version__}. Please upgrade pandas to the minimum supported version."
+    )
+
+if parse(np.__version__) < _min_numpy_version:
+    raise ImportError(
+        "pandera requires numpy >= 1.24.4, but you have numpy "
+        f"{np.__version__}. Please upgrade numpy to the minimum supported version."
+    )
+
+
+warnings.warn(
+    "The pandera module for pandas data validation has been moved to "
+    "`pandera.pandas`. Please update your import statement to "
+    "`import pandera.pandas as pa`.",
+    FutureWarning,
+)
+
 
 from pandera._patch_numpy2 import _patch_numpy2
-
 
 _patch_numpy2()
 
