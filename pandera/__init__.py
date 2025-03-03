71a9492fc2dc810a6b796d96f971f5c5c7cf0f9e
cosmicBboy <niels.bantilan@gmail.com>
2025-03-02 21:45:30 -0500

    clean up pyproject.toml
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- pandera/__init__.py
+++ pandera/__init__.py
@@ -2,19 +2,47 @@
 """A flexible and expressive dataframe validation library."""
 
 import sys
+import warnings
+from packaging.version import parse
 
 
 try:
     import pandas as pd
 except ImportError:
-    PANDAS_INSTALLED = False
+    _pandas_installed = False
 else:
-    PANDAS_INSTALLED = True
+    _pandas_installed = True
+    import numpy as np
+
     # check minimum pandas version
 
 
-if not PANDAS_INSTALLED:
+if not _pandas_installed:
     sys.exit(0)
 
+
+_min_pandas_version = parse("2.1.1")
+_min_numpy_version = parse("1.24.4")
+
+
+if parse(pd.__version__) < _min_pandas_version:
+    raise ImportError(
+        "pandera requires pandas >= 2.1.1, but you have pandas "
+        f"{pd.__version__}. Please upgrade pandas to the minimum supported version."
+    )
+
+if parse(np.__version__) < _min_numpy_version:
+    raise ImportError(
+        "pandera requires numpy >= 1.24.4, but you have numpy "
+        f"{np.__version__}. Please upgrade numpy to the minimum supported version."
+    )
+
+
+warnings.warn(
+    "The pandera module for pandas data validation has been moved to "
+    "`pandera.pandas`. Please update your import statement to "
+    "`import pandera.pandas as pa`.",
+    FutureWarning,
+)
 
 from pandera.pandas import *
