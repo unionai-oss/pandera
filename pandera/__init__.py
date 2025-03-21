321fbdc6f26b91cdbc9f372593be217136ddc451
Lundy Bernard <lundybernard@users.noreply.github.com>
2025-03-21 07:15:36 -0700

    Fix exit-0 issue in pandera.__init__ (#1947)
    * Remove exit-0 call from pandera.__init__.py
    Include pandera.pandas symbols in pandera namespace,
    only if pandas and numpy are installed
    
    * add deprecation warning for pandas dependency

--- pandera/__init__.py
+++ pandera/__init__.py
@@ -1,17 +1,32 @@
 # pylint: disable=wrong-import-position
 """A flexible and expressive dataframe validation library."""
 
-import sys
+
+from logging import getLogger
 
 from pandera._version import __version__
 
+
+log = getLogger(__name__)
+
+
 try:
+    #  Only add pandas to the pandera namespace
+    #  if pandas and numpy are installed
     import pandas as pd
-except ImportError:
-    sys.exit(0)
+    import numpy as np
 
+    from pandera.pandas import *
+    from pandera.pandas import __all__
+    __all__.append("__version__")
 
-from pandera.pandas import *
-from pandera.pandas import __all__
+except ImportError as err:
+    if 'pandas' or 'numpy' in str(err):
+        log.warn(
+            'Pandas and Numpy are required for this version of pandera.',
+            err,
+        )
+    else:
+        raise  # Re-raise any other `ImportError` exceptions
 
-__all__.append("__version__")
+    __all__ = ["__version__", ]
