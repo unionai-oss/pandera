215f86d42e281d78738354a9f02bb68be4ea356d
ksolarski <pchla10@gmail.com>
2025-04-11 17:05:13 +0200

    Fix missing column
    Signed-off-by: ksolarski <pchla10@gmail.com>

--- tests/polars/test_polars_components.py
+++ tests/polars/test_polars_components.py
@@ -275,5 +275,20 @@ def test_expr_as_default():
         "d": [1, 2, 3],
     }
 
+def test_missing_with_extra_columns():
+    schema = pa.DataFrameSchema(
+        columns={
+            "a": pa.Column(int),
+            "b": pa.Column(float, default=1),
+        },
+        add_missing_columns=True,
+        coerce=True,
+    )
+    df = pl.LazyFrame({"a": [1, 2, 3], "c": [4, 5, 6]})
+    assert schema.validate(df).collect().to_dict(as_series=False) == {
+        "a": [1, 2, 3],
+        "b": [1.0, 1.0, 1.0],
+        "c": [4, 5, 6],
+    }
 
 def test_column_schema_on_lazyframe_coerce(): ...
