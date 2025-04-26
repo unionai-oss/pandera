b735ccb2a43e9bc85c23b1b2b617da693090e6b5
cosmicBboy <niels.bantilan@gmail.com>
2025-04-25 20:35:31 -0400

    built-in checks support table-level checks
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- pandera/constants.py
+++ pandera/constants.py
@@ -1,4 +1,6 @@
 """Pandera constants."""
 
 CHECK_OUTPUT_KEY = "check_output"
+CHECK_OUTPUT_SUFFIX = f"__{CHECK_OUTPUT_KEY}__"
 FAILURE_CASE_KEY = "failure_case"
+check_col_name = f"{{col}}{CHECK_OUTPUT_SUFFIX}"
