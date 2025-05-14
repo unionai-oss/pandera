32131e851c5404a7d2503837d13d5eecd1d2c3b7
cosmicBboy <niels.bantilan@gmail.com>
2025-05-14 19:49:00 -0400

    drop_invalid_rows still raises schema-level errors
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- pandera/backends/pandas/base.py
+++ pandera/backends/pandas/base.py
@@ -189,27 +189,16 @@ class PandasSchemaBackend(BaseSchemaBackend):
 
     def drop_invalid_rows(self, check_obj, error_handler: ErrorHandler):
         """Remove invalid elements in a check obj according to failures caught by the error handler."""
-        errors = error_handler.schema_errors
-        for err in errors:
+        for err in error_handler.schema_errors:
+            if isinstance(err.failure_cases, str):
+                # if the failure cases are a string, it means the error is
+                # a schema-level error.
+                continue
             if isinstance(check_obj.index, pd.MultiIndex):
-                # MultiIndex values are saved on the error as strings so need to be cast back
-                # to their original types
-                if isinstance(err.failure_cases, str):
-                    # string type failure cases indicates that the entire column/dataframe
-                    # being checked didn't pass validation, meaning the entire dataframe
-                    # is invalid.
-                    index_values = check_obj.index
-                else:
-                    index_tuples = err.failure_cases["index"].apply(eval)
-                    index_values = pd.MultiIndex.from_tuples(index_tuples)
+                index_tuples = err.failure_cases["index"].apply(eval)
+                index_values = pd.MultiIndex.from_tuples(index_tuples)
             else:
-                if isinstance(err.failure_cases, str):
-                    # string type failure cases indicates that the entire column/dataframe
-                    # being checked didn't pass validation, meaning the entire dataframe
-                    # is invalid.
-                    index_values = check_obj.index
-                else:
-                    index_values = err.failure_cases["index"]
+                index_values = err.failure_cases["index"]
 
             mask = ~check_obj.index.isin(index_values)
             check_obj = check_obj.loc[mask]
