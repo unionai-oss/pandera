c71393806a7e5af268f89e8a1f9fd06dfba7a277
Niels Bantilan <niels.bantilan@gmail.com>
2025-06-03 16:49:47 -0400

    delete foo.py
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- foo.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import pandera.pandas as pa
-
-
-df = pd.DataFrame({"col1": ["a", "b"]}, dtype="string[pyarrow]")
-schema = pa.DataFrameSchema(columns={"col1": pa.Column("string[pyarrow]")})
-schema.validate(df)
