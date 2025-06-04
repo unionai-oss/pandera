443fd6c5744b1acc6eee245edab18444e96a253f
cosmicBboy <niels.bantilan@gmail.com>
2025-06-03 21:20:25 -0400

    pin geopandas
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -254,8 +254,21 @@ class FilterTypeAnnotationWarnings(pylogging.Filter):
         )
 
 
+class FilterExternalPackageModinDocUtilsWarnings(pylogging.Filter):
+    def filter(self, record: pylogging.LogRecord) -> bool:
+        return not (
+            "modin" in record.location
+            and record.getMessage().startswith(
+                "Inline strong start-string without end-string",
+            )
+        )
+
+
 logging.getLogger("sphinx_autodoc_typehints").logger.addFilter(
     FilterTypeAnnotationWarnings()
+)
+logging.getLogger("sphinx.util.docutils").logger.addFilter(
+    FilterExternalPackageModinDocUtilsWarnings()
 )
 
 
