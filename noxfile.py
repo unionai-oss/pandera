3aab68ef0fd6e38191142f52c85f8cc77e9291fc
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-10-29 15:21:23 -0600

    Add 3.14 to the list of Python versions in noxfile
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- noxfile.py
+++ noxfile.py
@@ -20,7 +20,7 @@ nox.options.sessions = (
     "docs",
 )
 
-PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
+PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13", "3.14"]
 PANDAS_VERSIONS = ["2.1.1", "2.2.3"]
 PYDANTIC_VERSIONS = ["1.10.11", "2.10.6"]
 POLARS_VERSIONS = ["0.20.0", "1.33.1"]
