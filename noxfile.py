974d40837efcb60cc5127a74c53b8a864c531a62
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-10-30 19:32:10 -0600

    Upgrade Pydantic to 2.12.3 for Python 3.14 support
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,7 @@ nox.options.sessions = (
 
 PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13", "3.14"]
 PANDAS_VERSIONS = ["2.1.1", "2.2.3"]
-PYDANTIC_VERSIONS = ["1.10.11", "2.10.6"]
+PYDANTIC_VERSIONS = ["1.10.11", "2.12.3"]
 POLARS_VERSIONS = ["0.20.0", "1.33.1"]
 PACKAGE = "pandera"
 SOURCE_PATHS = PACKAGE, "tests", "noxfile.py"
