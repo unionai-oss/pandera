34dbe2d2ce9c82119990cb1eaa348def970a1c9b
Deepyaman Datta <deepyaman.datta@utexas.edu>
2025-11-17 10:14:42 -0700

    Implement `drop_invalid_rows` for the Ibis backend (#2151)
    * Implement `check_nullable` checks for Ibis backend
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Test whether backend supports `IsNan` before using
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Use public `ibis.get_backend` over `_find_backend`
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Implement `drop_invalid_rows` for the Ibis backend
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Implement workaround for backends without pos join
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    * Test `drop_invalid_rows` without a positional join
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>
    
    ---------
    
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- pandera/backends/ibis/checks.py
+++ pandera/backends/ibis/checks.py
@@ -10,17 +10,13 @@ from pandera.api.base.checks import CheckResult
 from pandera.api.checks import Check
 from pandera.api.ibis.types import IbisData
 from pandera.backends.base import BaseCheckBackend
+from pandera.backends.ibis.constants import POSITIONAL_JOIN_BACKENDS
 from pandera.backends.ibis.utils import select_column
 from pandera.constants import CHECK_OUTPUT_KEY, CHECK_OUTPUT_SUFFIX
 
 if TYPE_CHECKING:
     import ibis.expr.types as ir
     from ibis.expr.types.groupby import GroupedTable
-
-
-# Manually maintained list derived from
-# https://github.com/ibis-project/ibis/blob/10.6.0/ibis/backends/tests/test_join.py#L370-L399
-POSITIONAL_JOIN_BACKENDS = {"duckdb", "polars"}
 
 
 class IbisCheckBackend(BaseCheckBackend):
@@ -184,8 +180,7 @@ class IbisCheckBackend(BaseCheckBackend):
         """Postprocesses the result of applying the check function."""
         passed = check_output[CHECK_OUTPUT_KEY].all()
         failure_cases = check_output.filter(~_[CHECK_OUTPUT_KEY]).drop(
-            s.endswith(f"__{CHECK_OUTPUT_KEY}__")
-            | select_column(CHECK_OUTPUT_KEY)
+            s.endswith(CHECK_OUTPUT_SUFFIX) | select_column(CHECK_OUTPUT_KEY)
         )
         if check_obj.key is not None:
             failure_cases = failure_cases.select(check_obj.key)
