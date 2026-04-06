e123383c1ca380e76db6d08da1ef473854eacdd1
cosmicBboy <niels.bantilan@gmail.com>
2026-04-05 21:23:29 -0400

    add io serdes support for ibis
    Signed-off-by: cosmicBboy <niels.bantilan@gmail.com>

--- pandera/io/__init__.py
+++ pandera/io/__init__.py
@@ -5,5 +5,6 @@ Examples:
 - ``import pandera.io.pandas_io`` — pandas / modin / dask / pyspark.pandas API schemas
 - ``import pandera.io.polars_io`` — Polars API schemas
 - ``import pandera.io.pyspark_sql_io`` — PySpark SQL API schemas
+- ``import pandera.io.ibis_io`` — Ibis API schemas
 - ``import pandera.io.xarray_io`` — xarray API schemas
 """
